5527ce9ac00bbef6eca3cba8d330fcaa6c69ce6c
Ross Ingram <r@rossingram.com>
2024-05-12 13:44:57 -0700

    Update thunderbolt_setup.sh

--- thunderbolt_setup.sh
+++ thunderbolt_setup.sh
@@ -4,22 +4,31 @@
 echo "Navigating to Thunderbolt devices directory..."
 cd /sys/bus/thunderbolt/devices/domain0/0-0/
 
-# Display the contents of the authorized file
-echo "Displaying contents of the authorized file..."
+# Perform the actions for device 0-1
+echo "Setting up device 0-1..."
+cd 0-1
 authorized=$(cat authorized)
-
 if [ $authorized -eq 0 ]; then
-  # Set authorized to 1
-  echo "Setting authorized to 1..."
+  echo "Setting authorized to 1 for device 0-1..."
   echo 1 > authorized
-  # Confirm it is set to 1
-  echo "Confirming authorized is set to 1..."
   authorized=$(cat authorized)
 fi
-
-# Rescan PCI devices
 echo "Rescanning PCI devices..."
 echo 1 > /sys/bus/pci/rescan
+cd ..
+
+# Perform the actions for device 0-3
+echo "Setting up device 0-3..."
+cd 0-3
+authorized=$(cat authorized)
+if [ $authorized -eq 0 ]; then
+  echo "Setting authorized to 1 for device 0-3..."
+  echo 1 > authorized
+  authorized=$(cat authorized)
+fi
+echo "Rescanning PCI devices..."
+echo 1 > /sys/bus/pci/rescan
+cd ..
 
 echo "Script execution completed."
 
